109bcb8f262e66f071f369d167b558706f657ecc
vicccente5 <chubitomermoreno@gmail.com>
2025-09-30 20:53:13 -0300

    avances en base de datos

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,35 +1,35 @@
 CREATE DATABASE veterinario;
-GO
+go
 
-USE veterinario;
-GO
 
-CREATE TABLE Due�o (
-    id_due�o int PRIMARY KEY,
-    nombre NVARCHAR(100),
-    direccion NVARCHAR(100), telefono varchar(50),email nvarchar(100)
+CREATE TABLE Dueno (
+id_dueno INT PRIMARY KEY,nombre NVARCHAR(100) NOT NULL,
+direccion NVARCHAR(100),telefono VARCHAR(50),email NVARCHAR(100)
 );
 
 CREATE TABLE Mascota (
-    id_mascota INT PRIMARY KEY,
-    nombre NVARCHAR(100),
-    especie NVARCHAR(50),
-    raza NVARCHAR(50),
-    fecha_nacimiento date,
-    id_due�o INT NOT NULL,
-    CONSTRAINT fk_mascota_due�o FOREIGN KEY (id_due�o) -- �Correcci�n aqu�!
-    REFERENCES Due�o(id_due�o)
-    ON DELETE CASCADE
+id_mascota INT PRIMARY KEY, nombre NVARCHAR(100) NOT NULL,especie NVARCHAR(50),raza NVARCHAR(50),
+fecha_nacimiento DATE,id_dueno INT NOT NULL,
+CONSTRAINT fk_mascota_dueno FOREIGN KEY (id_dueno)
+REFERENCES Dueno(id_dueno)
+ON DELETE CASCADE
 );
 
-CREATE TABLE consulta (
-    id_consulta INT PRIMARY KEY,
-    fecha_consulta DATE DEFAULT GETDATE() NOT NULL, 
-    motivo NVARCHAR(200),
-    diagnostico NVARCHAR(200),
-    tratamiento NVARCHAR(200),
-    id_mascota INT NOT NULL,
-    CONSTRAINT fk_consulta_mascota FOREIGN KEY (id_mascota)
-    REFERENCES Mascota(id_mascota)
-    ON DELETE CASCADE
+CREATE TABLE Veterinario (
+id_veterinario INT PRIMARY KEY,nombre NVARCHAR(100) NOT NULL,
+especialidad NVARCHAR(100),telefono VARCHAR(50),email NVARCHAR(100)
 );
+
+CREATE TABLE Consulta (
+id_consulta INT PRIMARY KEY,fecha_consulta DATE DEFAULT GETDATE() NOT NULL,motivo NVARCHAR(200),
+diagnostico NVARCHAR(200),tratamiento NVARCHAR(200),observaciones NVARCHAR(300),id_mascota INT NOT NULL,
+id_veterinario INT,
+CONSTRAINT fk_consulta_mascota FOREIGN KEY (id_mascota)
+REFERENCES Mascota(id_mascota)
+ON DELETE CASCADE,
+CONSTRAINT fk_consulta_veterinario FOREIGN KEY (id_veterinario)
+REFERENCES Veterinario(id_veterinario)
+);
+
+
+
